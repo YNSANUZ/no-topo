458b69a57543f1820fffb4653dab17b37e30818d
Primus <primus.df@gmail.com>
2026-09-24 10:48:20 -0300

    Serve No Topo from nexo.yt

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
 };
 
 export default nextConfig;
